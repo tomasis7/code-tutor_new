ee773fb70a9b32d93e07c5021c56e7a4a3e5272e
Tomas Maldonis <tomasis7@gmail.com>
2024-11-16 14:40:07 +0100

    Add chat functionality and update command structure in Code Tutor extension

--- src/extension.ts
+++ src/extension.ts
@@ -1,26 +1,66 @@
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
-import * as vscode from 'vscode';
+import * as vscode from "vscode";
 
 // This method is called when your extension is activated
 // Your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
+  // Use the console to output diagnostic information (console.log) and errors (console.error)
+  // This line of code will only be executed once when your extension is activated
+  console.log('Congratulations, your extension "code-tutor" is now active!');
 
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "code-tutor" is now active!');
+  // The command has been defined in the package.json file
+  // Now provide the implementation of the command with registerCommand
+  // The commandId parameter must match the command field in package.json
+  const disposable = vscode.commands.registerCommand(
+    "code-tutor.helloWorld",
+    () => {
+      // The code you place here will be executed every time your command is executed
+      // Display a message box to the user
+      vscode.window.showInformationMessage("Hello World from Code Tutor!");
+    },
+  );
 
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
-	const disposable = vscode.commands.registerCommand('code-tutor.helloWorld', () => {
-		// The code you place here will be executed every time your command is executed
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from Code Tutor!');
-	});
-
-	context.subscriptions.push(disposable);
+  context.subscriptions.push(disposable);
 }
 
 // This method is called when your extension is deactivated
 export function deactivate() {}
+//
+const BASE_PROMPT =
+  "You are a helpful code tutor. Your job is to teach the user with simple descriptions and sample code of the concept. Respond with a guided overview of the concept in a series of messages. Do not give the user the answer directly, but guide them to find the answer themselves. If the user asks a non-programming question, politely decline to respond.";
+// define a chat handler
+const handler: vscode.ChatRequestHandler = async (
+  request: vscode.ChatRequest,
+  context: vscode.ChatContext,
+  stream: vscode.ChatResponseStream,
+  token: vscode.CancellationToken,
+) => {
+  // initialize the prompt
+  let prompt = BASE_PROMPT;
+
+  // initialize the messages array with the prompt
+  const messages = [vscode.LanguageModelChatMessage.User(prompt)];
+
+  // add in the user's message
+  messages.push(vscode.LanguageModelChatMessage.User(request.prompt));
+
+  // send the request
+  const chatResponse = await request.model.sendRequest(messages, {}, token);
+
+  // stream the response
+  for await (const fragment of chatResponse.text) {
+    stream.markdown(fragment);
+  }
+
+  return;
+};
+
+// create participant
+const tutor = vscode.chat.createChatParticipant(
+  "chat-tutorial.code-tutor",
+  handler,
+);
+
+// add icon to participant
+tutor.iconPath = vscode.Uri.joinPath(context.extensionUri, "tutor.jpeg");
